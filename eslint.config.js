1616079733759b2e143e95df30c6b5ee148b89e6
Christhofer <christhofer@gradin.co.id>
2026-03-05 11:11:19 +0700

    move eslint formating to stylistic plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import ts from 'typescript-eslint'
 
@@ -7,23 +8,33 @@ export default [
 
   // Js
   js.configs.recommended,
+  // {
+  //   rules: {
+  //     'no-undef': 'off',
+  //   },
+  // },
+
+  // Stylistic
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
-      'no-undef': 'off',
-      'indent': ['warn', 2],
-      'comma-dangle': ['warn', 'always-multiline'],
-      'func-call-spacing': 'warn',
-      'space-in-parens': ['warn', 'never'],
-      'space-infix-ops': 'warn',
-      'no-multi-spaces': 'warn',
-      'key-spacing': 'warn',
-      'no-whitespace-before-property': 'warn',
-      'computed-property-spacing': ['warn', 'never'],
-      'arrow-spacing': 'warn',
-      'object-curly-spacing': ['warn', 'always'],
-      'keyword-spacing': 'warn',
-      'space-before-blocks': 'warn',
-      'comma-spacing': 'warn',
+      '@stylistic/indent': ['warn', 2],
+      '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+      '@stylistic/function-call-spacing': 'warn',
+      '@stylistic/space-in-parens': ['warn', 'never'],
+      '@stylistic/space-infix-ops': 'warn',
+      '@stylistic/no-multi-spaces': 'warn',
+      '@stylistic/key-spacing': 'warn',
+      '@stylistic/no-whitespace-before-property': 'warn',
+      '@stylistic/computed-property-spacing': ['warn', 'never'],
+      '@stylistic/arrow-spacing': 'warn',
+      '@stylistic/object-curly-spacing': ['warn', 'always'],
+      '@stylistic/keyword-spacing': 'warn',
+      '@stylistic/space-before-blocks': 'warn',
+      '@stylistic/comma-spacing': 'warn',
+      '@stylistic/type-generic-spacing': 'warn',
     },
   },
 
